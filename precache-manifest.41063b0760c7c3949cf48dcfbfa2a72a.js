6f8b08a552b88c203bf8430c8b60cb69f29d0899
funsujan <107162347+funsujan@users.noreply.github.com>
2025-03-09 22:30:35 -0400

    Updates

---
+++ precache-manifest.41063b0760c7c3949cf48dcfbfa2a72a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c2560d640a941aebfda91035337b36",
+    "url": "/comp2156/index.html"
+  },
+  {
+    "revision": "84a31b08213356d65b19",
+    "url": "/comp2156/static/css/main.179e7dee.chunk.css"
+  },
+  {
+    "revision": "f5d5f0263be61aec9df1",
+    "url": "/comp2156/static/js/2.e587c0bb.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/comp2156/static/js/2.e587c0bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b26eb387e785008380",
+    "url": "/comp2156/static/js/3.2fa9004c.chunk.js"
+  },
+  {
+    "revision": "84a31b08213356d65b19",
+    "url": "/comp2156/static/js/main.afa291a7.chunk.js"
+  },
+  {
+    "revision": "1b31dc5ae46c33a6fa53",
+    "url": "/comp2156/static/js/runtime-main.3088a121.js"
+  }
+]);
